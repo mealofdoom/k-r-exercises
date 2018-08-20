d3a416d1ea8603820424172035ad9ec166277c1c
Lucas Corssac <lcorssac@hotmail.com>
2018-08-20 12:34:25 -0300

    Starting Commit

---
+++ 1.6 - Arrays/exercise 1-13.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outinside a word */
+
+#define MAXWORDSIZE 100
+
+
+/*count lines, words, and characters in input */
+
+int main ()
+{
+    int wordcounts[MAXWORDSIZE];
+    int wordsize;
+    int i,j;
+    int c, nl, nw, nc, state;
+    
+    state = OUT;
+    nl = nw = nc = 0;
+
+    for (i = 0; i < MAXWORDSIZE; ++i)
+        wordcounts[i] = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if ( c == ' ' || c == '\n' || c == '\t')
+        {
+            if (state == IN)
+            {
+                wordcounts[wordsize]++;
+            }
+            state = OUT;
+            wordsize = 0;
+        }
+        else if (state == IN)
+            ++wordsize;
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+            ++wordsize;
+        }
+    }
+
+    for (i = 0; i < MAXWORDSIZE; ++i)
+    {
+        for (j = 0; j < wordcounts[i]; ++j)
+        {
+            putchar('=');
+        }
+        printf("\n");   
+    }
+}
